aebf89f4a0d9ce544e165a391d03814f251de474
Mohamed-Gaafar9889 <148647095+Mohamed-Gaafar9889@users.noreply.github.com>
2024-02-27 03:54:09 +0200

    Add files via upload

---
+++ Begginer_Level_Projects/Pendulum_Motion/Embedded_Coder_Files/rtmodel.h
@@ -0,0 +1,27 @@
+/*
+ * File: rtmodel.h
+ *
+ * Code generated for Simulink model 'Temperature_Control_System'.
+ *
+ * Model version                  : 1.1
+ * Simulink Coder version         : 23.2 (R2023b) 01-Aug-2023
+ * C/C++ source code generated on : Mon Feb 26 23:38:03 2024
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Atmel->AVR
+ * Code generation objectives:
+ *    1. Execution efficiency
+ *    2. RAM efficiency
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_rtmodel_h_
+#define RTW_HEADER_rtmodel_h_
+#include "Temperature_Control_System.h"
+#endif                                 /* RTW_HEADER_rtmodel_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
