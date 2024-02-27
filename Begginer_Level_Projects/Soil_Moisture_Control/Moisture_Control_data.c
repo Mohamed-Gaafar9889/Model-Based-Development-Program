fda76f7830f4c66bfd58f5134fea33179ca51a8a
Mohamed-Gaafar9889 <148647095+Mohamed-Gaafar9889@users.noreply.github.com>
2024-02-27 03:48:11 +0200

    Add files via upload

---
+++ Begginer_Level_Projects/Soil_Moisture_Control/Moisture_Control_data.c
@@ -0,0 +1,37 @@
+/*
+ * File: Moisture_Control_data.c
+ *
+ * Code generated for Simulink model 'Moisture_Control'.
+ *
+ * Model version                  : 1.3
+ * Simulink Coder version         : 23.2 (R2023b) 01-Aug-2023
+ * C/C++ source code generated on : Tue Feb 27 03:36:57 2024
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Atmel->AVR (32-bit)
+ * Code generation objectives:
+ *    1. Execution efficiency
+ *    2. RAM efficiency
+ * Validation result: Not run
+ */
+
+#include "Moisture_Control.h"
+
+/* Constant parameters (default storage) */
+const ConstP rtConstP = {
+  /* Expression: [0 5 10 15 20 25 30]
+   * Referenced by: '<S6>/2-D Lookup Table1'
+   */
+  { 0.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0 },
+
+  /* Expression: [0 0.1 0.2 0.3 0.4 0.5 0.6]
+   * Referenced by: '<S6>/2-D Lookup Table1'
+   */
+  { 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6 }
+};
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
